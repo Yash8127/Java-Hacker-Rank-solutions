016ef6a21f8c5ca9ef3dbd87c3b04735978a99fe
Yash8127 <yaswanthgurindapalli@gmail.com>
2026-02-10 11:47:16 +0530

    Added Day-02 solutions

---
+++ Day-02/JavaLoops1.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    
+    public static void table(int n){
+        for(int i=1;i<=10;i++){
+            System.out.println(n + " x "+i+" = "+(n*i));
+        }
+    }
+    
+    
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        table(N);
+
+        bufferedReader.close();
+    }
+}
