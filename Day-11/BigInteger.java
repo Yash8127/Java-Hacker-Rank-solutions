12e6fd16abbc720c72f7ffee0dfa9713c5502b2c
Yash8127 <yaswanthgurindapalli@gmail.com>
2026-03-03 21:20:49 +0530

    solved problems

---
+++ Day-11/BigInteger.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+
+        bufferedReader.close();
+        BigInteger number = new BigInteger(n);
+
+        if (number.isProbablePrime(100)) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+    }
+}
